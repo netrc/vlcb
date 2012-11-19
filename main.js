4b41edcd7bac79f88f11c1372d87a02ad19894b8
richard <ric@netrc.com>
2012-11-19 16:22:03 -0500

    mon final2

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9\n');
-}).listen(process.env.PORT);
+// npm install express
+// npm install jade
+// npm install mongolian
+
+var express = require('express')
+  , a = require('jade')
+  , app = express();
+
+console.log(express.json());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser('foobar'));
+app.use(express.session());
+app.use(express.static('views'));   // will check this for pages...
+
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+  res.render('index.jade', {title: 'vlc'});
+});
+
+app.listen(process.env.PORT);
+console.log('listening on '+ process.env.PORT);
